2e079bfcdf9153ab656829e4035c610aa6f4c51d
drpsnt <enduro.kexxx@gmail.com>
2023-02-03 01:35:54 +0200

    create menu burger

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+$(document).ready(function (){
+    $('.header_burger').click(function (event){
+        $('.header_menu,.header_burger').toggleClass('burger_active');
+        $('body').toggleClass('no-scroll');
+    })
+});
+
 $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
